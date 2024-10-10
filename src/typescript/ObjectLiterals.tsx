28d9f2f6ffda4f0a9fa52bf006c41dddfc72c459
Carlos Verdugo <carlos.versa99@gmail.com>
2024-10-09 22:16:01 -0700

    Adding object literals

--- src/typescript/ObjectLiterals.tsx
+++ src/typescript/ObjectLiterals.tsx
@@ -1,7 +1,33 @@
+interface Person {
+    fullName: string;
+    lastName: string;
+    age: number;
+    address: Address;
+    isAlive?: boolean;
+}
+
+interface Address {
+    country: string;
+    houseNo: number;
+}
+
+
 export const ObjectLiterals = () => {
+    const person: Person = {
+        age: 25,
+        fullName: "",
+        lastName: '',
+        address: {
+            country: 'Canada',
+            houseNo: 615
+        }
+    }
+
   return (
     <div>
-      ObjectLiterals
+      <pre>
+        {JSON.stringify(person, null, 2)}
+      </pre>
     </div>
   )
 }
